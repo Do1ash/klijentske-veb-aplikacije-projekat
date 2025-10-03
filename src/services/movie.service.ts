b4973e37015bb1673f4c38ae56f205cb460b135a
Do1ash <sveta.dolasevic@gmail.com>
2025-10-03 19:40:55 +0200

    update

---
+++ src/services/movie.service.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const client = axios.create({
+    baseURL: 'https://movie.pequla.com/api',
+    headers: {
+        'Accept': 'application/json',
+        'X-Client-Name':'KVA/2025'
+    },
+    validateStatus:(status: number) =>{
+        return status === 200
+    }
+})
+
+export class MovieService{
+    static async getMovies(page: number = 0, size: number = 10){
+        return client.request({
+            url:'/movie',
+            method:'GET',
+            params:{
+                'page':page,
+                'size':size,
+                'sort': 'createdAt,desc'
+            }
+        })
+    }
+
+    static async getMoviesByID(id:number){
+        return axios.get('/movie/'+ id)
+    }
+}
