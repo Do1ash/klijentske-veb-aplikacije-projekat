74f894e68bb47903e7aba1e98217e4483a2bdb1b
Do1ash <sveta.dolasevic@gmail.com>
2025-10-03 19:38:19 +0200

    UI stylized and added some basic functions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import {  NgFor, NgIf } from '@angular/common';
+import { AxiosError } from 'axios';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MovieModel } from '../../models/movie.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgIf,NgFor,MatCardModule,MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  movies: any | null = null
+  error: string | null = null
+  
+  constructor(){
+    MovieService.getMovies(0, 3)
+      .then(rsp=> this.movies = rsp.data)
+      .catch((e: AxiosError)=> this.error = `${e.code}:${e.message}`)
+  }
 }
